501569abb36729c49ed5b98cce502b0e24b5a1bf
donspb <donspb@gmail.com>
2021-06-16 20:39:58 +0300

    practice 10

--- app/src/main/java/ru/gb/androidone/donspb/cinematron/view/MainFragment.kt
+++ app/src/main/java/ru/gb/androidone/donspb/cinematron/view/MainFragment.kt
@@ -1,28 +1,49 @@
 package ru.gb.androidone.donspb.cinematron.view
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import ru.gb.androidone.donspb.cinematron.Consts
 import ru.gb.androidone.donspb.cinematron.R
 import ru.gb.androidone.donspb.cinematron.databinding.MainFragmentBinding
+import ru.gb.androidone.donspb.cinematron.map.MapsFragment
 import ru.gb.androidone.donspb.cinematron.model.MovieListItem
 import ru.gb.androidone.donspb.cinematron.viewmodel.AppState
 import ru.gb.androidone.donspb.cinematron.viewmodel.LocalViewModel
 import ru.gb.androidone.donspb.cinematron.viewmodel.MainViewModel
 import ru.gb.androidone.donspb.cinematron.viewmodel.MovieListsEnum
+import java.io.IOException
 
-private const val RECENT_LIST_ID = 0
+private const val REQUEST_CODE = 85
+private const val REFRESH_PERIOD = 60000L
+private const val MINIMAL_DISTANCE = 100f
 
 class MainFragment : Fragment() {
 
     private var _binding: MainFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private val onLocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            context?.let {
+                getAddressAsync(it, location)
+            }
+        }
+    }
 
     private val viewModel: MainViewModel by lazy {
         ViewModelProvider(this).get(MainViewModel::class.java)
@@ -67,8 +88,8 @@ class MainFragment : Fragment() {
             movieRecyclerTop.adapter = adapterTop
             movieRecyclerPop.adapter = adapterPop
             movieRecyclerUp.adapter = adapterUp
+            mainFragmentLocationFAB.setOnClickListener { checkPermission() }
         }
-
 
         viewModelRecent.recentLiveData.observe(viewLifecycleOwner, Observer {
             renderData(it, R.string.recent_list)
@@ -137,6 +158,170 @@ class MainFragment : Fragment() {
 
     interface OnItemViewClickListener {
         fun onItemViewClick(movie: MovieListItem)
+    }
+
+    private fun checkPermission() {
+        activity?.let {
+            when {
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED -> {
+                            getLocation()
+                        }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) ->
+                {
+                    showRationaleDialog()
+                }
+                else -> {
+                    requestPermission()
+                }
+            }
+        }
+    }
+
+    private fun showRationaleDialog() {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_rationale_title))
+                .setMessage(getString(R.string.dialog_rationale_message))
+                .setPositiveButton(getString(R.string.dialog_rationale_give_access))
+                { _, _ -> requestPermission() }
+                .setNegativeButton(getString(R.string.dialog_rationale_decline))
+                {dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun requestPermission() {
+        requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        checkPermissionsResult(requestCode, grantResults)
+    }
+
+    private fun checkPermissionsResult (requestCode: Int, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE -> {
+                var grantedPermissions = 0
+                if ((grantResults.isNotEmpty())) {
+                    for (i in grantResults) {
+                        if (i == PackageManager.PERMISSION_GRANTED) {
+                            grantedPermissions++
+                        }
+                    }
+                    if (grantResults.size == grantedPermissions) {
+                        getLocation()
+                    } else {
+                        showDialog(
+                            getString(R.string.dialog_title_no_gps),
+                            getString(R.string.dialog_message_no_gps)
+                        )
+                    }
+                } else {
+                    showDialog(
+                        getString(R.string.dialog_title_no_gps),
+                        getString(R.string.dialog_message_no_gps)
+                    )
+                }
+                return
+            }
+        }
+    }
+
+    private fun showDialog(title: String, message: String) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(title)
+                .setMessage(message)
+                .setNegativeButton(getString(R.string.dialog_button_close))
+                { dialog, _ -> dialog.dismiss() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun getLocation() {
+        activity?.let { context ->
+            if (ContextCompat.checkSelfPermission(
+                    context, Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+            {
+                val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as
+                        LocationManager
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+                    provider?.let {
+                        locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            REFRESH_PERIOD,
+                            MINIMAL_DISTANCE,
+                            onLocationListener
+                        )
+                    }
+                } else {
+                    val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if (location == null) {
+                        showDialog(
+                            getString(R.string.dialog_title_gps_off),
+                            getString(R.string.dialog_message_last_loc_unknown)
+                        )
+                    } else {
+                        getAddressAsync(context, location)
+                        showDialog(
+                            getString(R.string.dialog_title_gps_off),
+                            getString(R.string.dialog_message_last_loc_known)
+                        )
+                    }
+                }
+            } else {
+                showRationaleDialog()
+            }
+        }
+    }
+
+    private fun getAddressAsync(context: Context, location: Location) {
+        val geoCoder = Geocoder(context)
+        Thread {
+            try {
+                val address = geoCoder.getFromLocation(
+                    location.latitude,
+                    location.longitude,
+                    1
+                )
+                binding.mainFragmentLocationFAB.post {
+                    showAddressDialog(address[0].getAddressLine(0), location)
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+
+    private fun showAddressDialog(address: String, location: Location) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.dialog_address_title))
+                .setMessage(address)
+                .setNegativeButton(getString(R.string.dialog_button_close))
+                { dialog, _ -> dialog.dismiss() }
+                .setPositiveButton(getString(R.string.dialog_open_map))
+                { _, _ ->
+                    activity?.supportFragmentManager?.apply {
+                        beginTransaction()
+                            .replace(R.id.main_container, MapsFragment())
+                            .addToBackStack("")
+                            .commitAllowingStateLoss()
+                    }
+                }
+                .create()
+                .show()
+        }
     }
 }
 
